db3421a7870a15059ec2b4a217df995b005d0da3
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-03-13 17:35:08 +0000

    enhancements to refesh process

--- refreshTools.sh
+++ refreshTools.sh
@@ -5,7 +5,19 @@
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $here/config.ini
 
+cd $here
+
+sftp -i ~/.ssh/ukmon $LOCATION@$UKMONHELPER << EOF
+get ukmon.ini
+get live.key
+get archive.key
+exit
+EOF
+chmod 0600 live.key archive.key
+
 echo "refreshing toolset"
 git stash 
 git pull
 git stash apply
+
+echo "done"
