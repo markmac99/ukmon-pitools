3af2369fe8df8f965e0e7cf2b5c8b0b977a77c66
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-03-18 09:10:13 +0000

    split liveMonitor so individual files can be manually sent

--- liveMonitor.py
+++ liveMonitor.py
@@ -1,63 +1,9 @@
 import time
 import os
 import sys
-import Utils.BatchFFtoImage as bff
 import boto3
-import shutil
-import tempfile
-
-
-
-def uploadOneEvent(cap_dir, dir_file, camloc, outf, s3):
-    outf.write('{:s} {:s} {:s}\n'.format(cap_dir, dir_file, camloc))
-    outf.flush()
-    target = 'ukmon-live'
-    spls = dir_file.split('_')
-    camid = spls[1]
-    ymd = spls[2]
-    hms = spls[3]
-    millis = spls[4]
-    yr = ymd[:4]
-    mth = ymd[4:6]
-    dy = ymd[6:8]
-    hr = hms[:2]
-    mi = hms[2:4]
-    se = hms[4:6]
-    se = se + '.{:.2f}'.format(float(millis)/1000)
-    tmpdir = tempfile.mkdtemp()
-    shutil.copy2(os.path.join(cap_dir, dir_file), tmpdir)
-    bff.batchFFtoImage(tmpdir, 'jpg')
-    file_name, _ = os.path.splitext(dir_file)
-    ojpgname = file_name + '.jpg'
-    njpgname = 'M' + ymd + '_' + hms + '_' + camloc + '_' + camid + 'P.jpg'
-    fulljpg = os.path.join(cap_dir, njpgname)
-    os.rename(os.path.join(cap_dir, ojpgname), fulljpg)
-
-    xmlname = 'M' + ymd + '_' + hms + '_' + camloc + '_' + camid + '.xml'
-    fullxml = os.path.join(cap_dir, xmlname)
-    with open(fullxml, 'w') as ofl:
-        ofl.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
-        ofl.write('<ufocapture_record version="215"')
-        ofl.write('y="{:s}" mo="{:s}" d="{:s}" h="{:s}" m="{:s}" s="{:s}" ',format(yr, mth, dy, hr, mi, se))
-        ofl.write('trig="1" frames="68" lng="LNG" lat="LAT" alt="ALTI" ')
-        ofl.write('tz="0" u2="224" cx="720" cy="576" fps="25.000" head="30" ')
-        ofl.write('tail="30" diff="2" sipos="6" sisize="15" dlev="40" dsize="4" ')
-        ofl.write('lid="{:s}" observer="" sid="{:s}" cam="{:s}" lens="" cap="" '.format(camloc, camid, camid))
-        ofl.write('comment="" interlace="1" bbf="0" dropframe="0">\n')
-        ofl.write('    <ufocapture_paths hit="3">\n')
-        ofl.write('     <uc_path fno="30" ono="18" pixel="3" bmax="79" x="395.7" y="282.3"></uc_path>\n')
-        ofl.write('     <uc_path fno="30" ono="18" pixel="9" bmax="96" x="393.6" y="288.1"></uc_path>\n')
-        ofl.write('     <uc_path fno="31" ono="18" pixel="16" bmax="112" x="391.1" y="295.5"></uc_path>\n')
-        ofl.write('    </ufocapture_paths>\n')
-        ofl.write('</ufocapture_record>\n')
-
-    s3.meta.client.upload_file(fulljpg, target, njpgname, ExtraArgs={'ContentType': 'image/jpeg'})
-    s3.meta.client.upload_file(fullxml, target, xmlname, ExtraArgs={'ContentType': 'application/xml'})
-    print(njpgname)
-    outf.write('{:s\n}'.format(njpgname))
-    outf.flush()
-    shutil.rmtree(tmpdir)
-    return
+import configparser
+import sendToLive as uoe
 
 
 def follow(thefile):
@@ -79,40 +25,43 @@ def follow(thefile):
 
 
 if __name__ == '__main__':
+    
     capdir = ''
     conn = boto3.Session() 
     s3 = conn.resource('s3')
     camloc = sys.argv[2]
-
-    outf = open('/home/pi/RMS_data/logs/liveMonitor.log', 'w')
     logfile = open(sys.argv[1],"r")
+
+    # read a few variables from the RMS config file
+    cfg = configparser.ConfigParser()
+    cfg.read('/home/pi/source/RMS/.config')
+    loc = []
+    loc.append(float(cfg['System']['Latitude'].split()[0]))
+    loc.append(float(cfg['System']['Longitude'].split()[0]))
+    loc.append(float(cfg['System']['Altitude'].split()[0]))
+    loc.append(cfg['System']['stationID'].split()[0])
+
+    # determine data directory
     while True:
         line = logfile.readline()
         if not line: 
             break
         if "Data directory" in line: 
-            capdir = line.split(' ')[5]
+            capdir = line.split(' ')[5].strip()
             print('capdir is', capdir)
-            outf.write('Capdir is {:s}\n'.format(capdir))
-            outf.flush()
             break
 
+    # rewind to start
     logfile.seek(0, 0)
-
     loglines = follow(logfile)
+
     # iterate over the generator
     for line in loglines:
         if "Data directory" in line: 
-            capdir = line.split(' ')[5]
-            # print('capdir is', capdir)
-            outf.write('Capdir is {:s}\n'.format(capdir))
-            outf.flush()
+            capdir = line.split(' ')[5].strip()
+            print('capdir is', capdir)
 
         if "detected meteors" in line and ": 0" not in line and "TOTAL" not in line:
             if capdir != '':
                 ffname = line.split(' ')[3]
-                outf.write('got event {:s}\n'.format(ffname))
-                outf.flush()
-                uploadOneEvent(capdir, ffname, camloc, outf, s3)
-
-    outf.close()
+                uoe.uploadOneEvent(capdir, ffname, camloc, s3, loc)
---
+++ sendToLive.py
@@ -0,0 +1,75 @@
+#
+# python script to upload one event to ukmon-live
+#
+import os
+import sys
+import Utils.BatchFFtoImage as bff
+import shutil
+import tempfile
+import boto3
+import configparser
+
+
+def uploadOneEvent(cap_dir, dir_file, camloc, s3=None, loc=None):
+    print('{:s} {:s} {:s}'.format(cap_dir, dir_file, camloc))
+
+    target = 'ukmon-live'
+    spls = dir_file.split('_')
+    camid = spls[1]
+    ymd = spls[2]
+    hms = spls[3]
+    millis = spls[4]
+    yr = ymd[:4]
+    mth = ymd[4:6]
+    dy = ymd[6:8]
+    hr = hms[:2]
+    mi = hms[2:4]
+    se = hms[4:6]
+    se = se + '.{:.2f}'.format(float(millis)/1000)
+    tmpdir = tempfile.mkdtemp()
+    shutil.copy2(os.path.join(cap_dir, dir_file), tmpdir)
+    bff.batchFFtoImage(tmpdir, 'jpg')
+    file_name, _ = os.path.splitext(dir_file)
+    ojpgname = file_name + '.jpg'
+    njpgname = 'M' + ymd + '_' + hms + '_' + camloc + '_' + camid + 'P.jpg'
+    fulljpg = os.path.join(cap_dir, njpgname)
+    os.rename(os.path.join(cap_dir, ojpgname), fulljpg)
+
+    xmlname = 'M' + ymd + '_' + hms + '_' + camloc + '_' + camid + '.xml'
+    fullxml = os.path.join(cap_dir, xmlname)
+    with open(fullxml, 'w') as ofl:
+        ofl.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
+        ofl.write('<ufocapture_record version="215"')
+        ofl.write('y="{:s}" mo="{:s}" d="{:s}" h="{:s}" m="{:s}" s="{:s}" ',format(yr, mth, dy, hr, mi, se))
+        ofl.write('trig="1" frames="68" lng="{:.4f}" lat="{:.4f}" alt="{:.1f}" '.format(loc[1], loc[0], loc[2]))
+        ofl.write('tz="0" u2="224" cx="1280" cy="720" fps="25.000" head="30" ')
+        ofl.write('tail="30" diff="2" sipos="6" sisize="15" dlev="40" dsize="4" ')
+        ofl.write('lid="{:s}" observer="" sid="{:s}" cam="{:s}" lens="" cap="" '.format(camloc, camid, camid))
+        ofl.write('comment="" interlace="1" bbf="0" dropframe="0">\n')
+        ofl.write('    <ufocapture_paths hit="3">\n')
+        ofl.write('     <uc_path fno="30" ono="18" pixel="3" bmax="79" x="395.7" y="282.3"></uc_path>\n')
+        ofl.write('     <uc_path fno="30" ono="18" pixel="9" bmax="96" x="393.6" y="288.1"></uc_path>\n')
+        ofl.write('     <uc_path fno="31" ono="18" pixel="16" bmax="112" x="391.1" y="295.5"></uc_path>\n')
+        ofl.write('    </ufocapture_paths>\n')
+        ofl.write('</ufocapture_record>\n')
+
+    s3.meta.client.upload_file(fulljpg, target, njpgname, ExtraArgs={'ContentType': 'image/jpeg'})
+    s3.meta.client.upload_file(fullxml, target, xmlname, ExtraArgs={'ContentType': 'application/xml'})
+    print(njpgname)
+    shutil.rmtree(tmpdir)
+    return
+
+
+if __name__ == '__main__':
+    conn = boto3.Session() 
+    s3 = conn.resource('s3')
+    # read a few variables from the RMS config file
+    cfg = configparser.ConfigParser()
+    cfg.read('/home/pi/source/RMS/.config')
+    loc = []
+    loc.append(float(cfg['System']['Latitude'].split()[0]))
+    loc.append(float(cfg['System']['Longitude'].split()[0]))
+    loc.append(float(cfg['System']['Altitude'].split()[0]))
+    loc.append(cfg['System']['stationID'].split()[0])
+
+    uploadOneEvent(sys.argv[1], sys.argv[2], sys.argv[3], s3, loc)
