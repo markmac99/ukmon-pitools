f6a6e20581dda60b6197613b2c9b072f96e457c1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-05-30 16:09:53 +0100

    Adding config and platepar to log collector

--- gatherLogs.sh
+++ gatherLogs.sh
@@ -8,12 +8,14 @@ source /home/pi/source/ukmon-pitools/ukmon.ini
 sudo cp /var/log/kern.log .
 sudo chown pi:pi kern.log
 cp /var/log/messages ./messages.log
+cp /home/pi/source/RMS/.config ${location}.config
+cp /home/pi/source/RMS/platepar_cmn2010.cal ${location}.cal
 ZIPFILE=/tmp/${LOCATION}_logs.tgz
-tar cvzf $ZIPFILE *.log*
+tar cvzf $ZIPFILE *.log* ${location}.config ${location}.cal
 sftp -i $UKMONKEY -q logupload@$UKMONHELPER << EOF
 cd logs
 progress
 put $ZIPFILE 
 exit
 EOF
-rm kern.log messages.log $ZIPFILE
+rm kern.log messages.log $ZIPFILE ${location}.config ${location}.cal
