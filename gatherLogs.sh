efe64017bf8e36cfdce4219371094b9727f6362c
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-07-01 18:30:36 +0100

    further small improvements to gatherlogs

--- gatherLogs.sh
+++ gatherLogs.sh
@@ -12,10 +12,12 @@ sudo chown pi:pi kern.log
 cp /var/log/messages ./messages.log
 cp /home/pi/source/RMS/.config ./${LOCATION}.config
 cp /home/pi/source/RMS/platepar_cmn2010.cal ./${LOCATION}.cal
+cp /home/pi/source/ukmon-pitools/*.key .
+cp /home/pi/source/ukmon-pitools/*.ini .
 crontab -l > ./crontab.txt
 find  .. -maxdepth 1 -name "*.log*" -type f -mtime -5 -exec cp {} . \;
 ZIPFILE=/tmp/${LOCATION}_logs.tgz
-tar cvzf $ZIPFILE *.log* ${LOCATION}.config ${LOCATION}.cal crontab.txt
+tar cvzf $ZIPFILE *.log* ${LOCATION}.config ${LOCATION}.cal crontab.txt *.key *.ini
 sftp -i $UKMONKEY -q logupload@$UKMONHELPER << EOF
 cd logs
 progress
