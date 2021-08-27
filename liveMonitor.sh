33f0f165a3cb91881e846c92f916540a0099cbb3
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-08-27 12:10:52 +0100

    dont run lifefeed if the location is not configured

--- liveMonitor.sh
+++ liveMonitor.sh
@@ -8,6 +8,11 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $here/ukmon.ini
 source $here/live.key
 
+if [ "$LOCATION" == "NOTCONFIGURED" ]; then
+    echo "station not configured, unable to continue" 
+    exit 1
+fi
+
 logf=`ls -1tr ~/RMS_data/logs/log*.log | tail -1 | head -1`
 logger "Monitoring $logf" -t $0
 
