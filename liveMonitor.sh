c663e05cc029009a14f442d781778fccf4fb86ba
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-01-23 18:13:50 +0000

    bugfix for killing too many things

--- liveMonitor.sh
+++ liveMonitor.sh
@@ -7,7 +7,7 @@ source ~/vRMS/bin/activate
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # kill any existing ukmon-live process
-pids=$(ps -ef | grep liveMonitor | egrep -v "grep|$$" | awk '{print $2}')
+pids=$(ps -ef | grep ${here}/liveMonitor | egrep -v "grep|$$" | awk '{print $2}')
 kill -9 $pids
 
 source $here/ukmon.ini
