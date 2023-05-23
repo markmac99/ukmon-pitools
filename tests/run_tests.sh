e1df122af7aa542cee55410c13d788c8d3143288
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-05-23 21:48:49 +0100

    switch to test keys so i can write to a nonprod envt

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,6 +6,6 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ~/vRMS/bin/activate
 export PYTHONPATH=$PYTHONPATH:$(here)
 cp ~/source/ukmon-pitools/ukmon.ini .
-cp ~/source/ukmon-pitools/live.key .
+cp ~/source/testing/live.key .
 pytest -v ./tests --cov=. --cov-report=term-missing
 rm ./live.key ./ukmon.ini
