e47ecc138f64bcc3b219533bf25a1c246b316b62
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-02-04 17:52:14 +0000

    tweak tests

--- tests/run_ub_tests.sh
+++ tests/run_ub_tests.sh
@@ -8,7 +8,7 @@ if [ ! -f /keys/ukmon.ini ] ; then
     echo no test config, aborting
     exit
 fi 
-cp /keys/.config /source/RMS/
+cp /keys/.config /root/source/RMS/
 cp /keys/ukmon.ini . 
 cp /keys/live.key .
 touch ./domp4s
